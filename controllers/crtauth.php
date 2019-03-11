fcee385a73b02ec2bbbf99b524598b817e67bbbc
juan jesus garcia centeno <juanjesusgarciacenteno@hotmail.com>
2019-03-11 10:43:01 -0600

    terminacion del apartado de usuarios
    -Validacion del modulo de seguridad
    -CBAM del usuario
    -Cambiar contraseña

--- controllers/crtauth.php
+++ controllers/crtauth.php
@@ -1,15 +1,55 @@
 <?php
 
-session_start();
 require_once("../models/mdlauth.php");
+
+session_start();
+
+//Variables post
 $username = filter_input(INPUT_POST, 'username');
 $password = filter_input(INPUT_POST, 'password');
-if (!empty($username) && !empty($password)) {
-    Mdlauth::Login($username, $password);
-    require '../views/vwmenuprincipal.php';
-} else {
+$closesesion = filter_input(INPUT_POST, 'closesesion');
+$changedpass = filter_input(INPUT_POST, 'changedpass');
+$newpassword = filter_input(INPUT_POST, 'newpassword');
+$valueforpassword = filter_input(INPUT_POST, 'valueforpassword');
+
+
+//Inicio de sesion
+ if (!empty($username) && !empty($password)) {
+    if (!isset($_SESSION['token']) && empty($_SESSION['token'])) {
+        Mdlauth::Login($username, $password);
+        echo '<script language = javascript>
+	self.location = "../views/vwmenuprincipal.php"
+	</script>';
+    }
+    else{
+       // Mdlauth::Logout();
+    }
+}
+ 
+//Se llama a la funcion cerrar sesion
+if ($closesesion && empty($username) && empty($password)) {
     Mdlauth::Logout();
 }
+//Se llama a la vista cambiar contraseña
+if (!empty($changedpass) && $changedpass) {
+    require_once("../views/users/vwalterpassword.php");
+}
+//Se llama la funcion cambiar contraseña
+if (!empty($valueforpassword) && $valueforpassword == 'valueforpassword' && !empty($newpassword)) {
+    Mdlauth::changedPassword($newpassword);
+    echo '<script language = javascript>
+	self.location = "../views/vwmenuprincipal.php"
+	</script>';
+}
+//Validacion de seguridad para cuando un usuario externo accede
+if (empty($closesesion) && empty($changedpass) && empty($password) && empty($username) && empty($valueforpassword) && empty($newpassword)) {
+    mdlsecurity::validationSecurity();
+}
+
+
+
+
+ 
 
 
 
--- views/users/vwmanageusers.php
+++ views/users/vwmanageusers.php
@@ -1,9 +1,10 @@
-<?php
-session_start();
-//echo $_SESSION['token'];
-require_once("../models/mdlsecurity.php");
-mdlsecurity::validateToken();
-?>
+<div hidden="true">
+    <?php
+    session_start();
+    require_once("C:/xampp/htdocs/finbaproject/FINBA/models/mdlsecurity.php");
+    mdlsecurity::validateToken();
+    ?>
+</div>
 <!DOCTYPE html>
 <html>
     <!-- head -->
@@ -11,7 +12,7 @@ mdlsecurity::validateToken();
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>FINBA</title>
+        <title>Administracion de Usuarios</title>
         <!-- Bootstrap CSS CDN -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
         <!-- Our Custom CSS -->
@@ -78,9 +79,8 @@ mdlsecurity::validateToken();
                                     </form>
                                 </li>
                                 <li>
-                                     <form action="../controllers/crtauth.php" method="post">
+                                    <form action="../controllers/crtauth.php" method="post">
                                         <div hidden="true">
-                                            <!-- Variable para cerrar sesion -->
                                             <input class="col-8" type="text" id="changedpass" name="changedpass" value="true">
                                         </div>
                                         <a href="javascript:;" onclick="parentNode.submit();">Cambiar Contraseña</a>
@@ -92,18 +92,17 @@ mdlsecurity::validateToken();
                         <li class="active">
                             <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                                 <i class="fas fa-tachometer"></i>
-                                Inventario
+                                Servicios
                             </a>
                             <ul class="collapse list-unstyled" id="homeSubmenu">
-                                      <li>
-                                          <form action="../controllers/crtinventory.php" method="post">
-                                        <div hidden="true">
-                                            <!-- Variable para cerrar sesion -->
-                                            <input class="col-8" type="text" id="searchinventory" name="searchinventory" value="true">
-                                        </div>
-                                        <a href="javascript:;" onclick="parentNode.submit();">Administrar Inventario</a>
-                                        <input type="hidden" name="mess" value= "">
-                                    </form>
+                                <li>
+                                    <a href="#"><i class="fas fa-tint"></i>Agua</a>
+                                </li>
+                                <li>
+                                    <a href="#">Home 2</a>
+                                </li>
+                                <li>
+                                    <a href="#">Home 3</a>
                                 </li>
                             </ul>
                         </li>
@@ -134,13 +133,93 @@ mdlsecurity::validateToken();
 
                 </nav>
             </div>
+            <div class="container">
+                <center><h2>Administracion de usuarios</h2></center>   
+                <center> <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Username(correo)</th>
+                                <th>Tipo de Usuario</th>
+                                <th>Eliminar Usuario</th>
+                                <th>Editar Usuario</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $listofusers;
+                            if (empty($listofusers)) {
+                                $voidarray = array();
+                                $listofusers = $voidarray;
+                            }
+                            for ($i = 0; $i < count($listofusers); $i++) {
+                                ?>
+                                <tr  class='btn-outline-primary'>
+                                    <td><?php echo $listofusers[$i]["username"]; ?></td>
+                                    <?php
+                                    if ($listofusers[$i]["type_user"] == 'Administrator') {
+                                        printf("<td height='80'  style='color: #EA1515 ;'><h5><b>Administrador</b></h5></td>");
+                                    } else if ($listofusers[$i]["type_user"] == 'Employee') {
+                                        printf("<td height='80'  style='color: blue ;'><h5><b>Empleado</b></h5></td>");
+                                    }
+                                    ?>
+                                    <td> 
+                                        <?php
+                                        $usernamedelete = $listofusers[$i]["username"];
+                                        $delete = '1';
+                                        $onydelete = '1';
+                                        echo '<a href="../controllers/crtusers.php?username=' . $usernamedelete . '&delete=' . $delete . '&onlydelete =' . $onydelete . '" '
+                                        . 'onclick="if (!confirm(\'Estas seguro que quieres eliminar este usuario?\')) '
+                                        . '{ return false}"><img src="../resources/img/eliminar.jpg"></a>'
+                                        ?></td>
+                                    <td><form method="post" action="../controllers/crtusers.php">
+                                            <?php
+                                            $usernameupdate = $listofusers[$i]["username"];
+                                            $typeuserupdate = $listofusers[$i]["type_user"];
+                                            if ($typeuserupdate == 'Administrator') {
+                                                $typeuserupdate = 'Administrador';
+                                            }
+                                            if ($typeuserupdate == 'Employee') {
+                                                $typeuserupdate = 'Empleado';
+                                            }
+                                            $update = '1';
+                                            echo "<input type='hidden' id='usernameupdate' name='usernameupdate' value='$usernameupdate'> "
+                                            . "<input type='hidden' id='update' name='update' value='$update'> "
+                                            . "<input type='hidden' id='typeuserupdate' name='typeuserupdate' value='$typeuserupdate'>"
+                                            . "<input type='submit' value='Actualizar'>"
+                                            ?>
+                                        </form></td>
+                                </tr>
+                                <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table></center>
+                <center>
+                    <form method="post" action="../controllers/crtusers.php" name="form1" enctype="multipart/form-data">
+                        <div class="modal-body">
+                            <div class="container">
+                                <center>
+                                    <div hidden="true">
+                                        <!-- value insert -->
+                                        <input class="col-8" type="text" id="insert" name="insert"  value="true">
+                                    </div>
+                                </center>
+                            </div>
+                        </div>
+                        <center><div class="modal-footer">
+                                <input type="submit" class="btn btn-primary"value="Registrar Usuario" >
+                            </div></center>
+                    </form>
+                </center> 
+            </div>  
             <!-- Page Content  -->
             <div id="content">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 </nav>
             </div>
         </div>
+        <div>
+        </div>
     </body>
+
 </html>
-
-
